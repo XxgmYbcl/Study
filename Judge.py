abeb77f85e84e5428335d4ef408e6b6c2a84f6f7
XxgmYbcl <he1151459078@gmail.com>
2019-02-12 22:25:07 +0800

    Create Judge.py
    input函数，平方、乘方、平方根

---
+++ Judge.py
@@ -0,0 +1,15 @@
+# -*- coding = utf-8 -*-
+
+import numpy
+import math
+
+s = int(input('birth: '))   #input后的括号，可以在你input前显示，作为一个提示
+print(s)                    #int()能把对应的数字字符串转化成数字，但是无法转换abc这种英文字符串
+
+print(pow(2,3))            #python自带的乘方函数
+print(pow(2,3,2))
+
+print(numpy.square(4))     #numpy的平方函数，square   
+
+print(math.sqrt(25))
+
